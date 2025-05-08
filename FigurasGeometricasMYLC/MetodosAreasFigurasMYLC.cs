4266b3ac6b73333203eb51256f235119450b2e37
JONASJMX <jonasjmx@gmail.com>
2025-05-08 10:16:36 -0500

    s2

---
+++ FigurasGeometricasMYLC/MetodosAreasFigurasMYLC.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasGeometricasMYLC
+{
+    public class Circle_MYLC : IAreaCalculator_MYLC
+    {
+        private double _radius;
+        public Circle_MYLC(double radius) => _radius = radius;
+
+        public double CalculateArea_MYLC() => Math.PI * _radius * _radius;
+    }
+
+    public class Square_MYLC : IAreaCalculator_MYLC
+    {
+        private double _side;
+        public Square_MYLC(double side) => _side = side;
+
+        public double CalculateArea_MYLC() => _side * _side;
+    }
+
+
+}

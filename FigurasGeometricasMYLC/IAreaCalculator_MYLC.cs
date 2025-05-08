5c91ab9452dd8654065a085f51824a3b99448a97
Walter Adrian Lopez Carrera <adrianwalterlopez@gmail.com>
2025-05-08 10:16:06 -0500

    s1

---
+++ FigurasGeometricasMYLC/IAreaCalculator_MYLC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurasGeometricasMYLC
+{
+    public interface IAreaCalculator_MYLC
+    {
+        double CalculateArea_MYLC();
+    }
+}
